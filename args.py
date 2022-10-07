7ff813faeb82391b7be1e9d741e48094b0760f5c
Simon Woodward <simon.woodward@zoopla.co.uk>
2022-10-07 15:23:51 +0100

    Accidental checkin

--- args.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("bob", type=int)
-parser.add_argument("--a", "--bob-bobodfdsfsdf", "--bodfb-bo", "-b", type=int)
-
-args = vars(parser.parse_args())
-print(f"{args}")
